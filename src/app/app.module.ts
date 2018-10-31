7b29c3cd684d4b3a2d1edfa745ba98e4d8b26290
Denys Panov <denys.panov@student.tuke.sk>
2018-10-31 20:55:32 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,27 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { IndexComponent } from './components/index/index.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {StoreModule} from '@ngrx/store';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatDividerModule, MatListModule, MatToolbarModule} from '@angular/material';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    IndexComponent
+    IndexComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    MatButtonModule,
     AppRoutingModule,
     ReactiveFormsModule
   ],
